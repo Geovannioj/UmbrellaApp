52073b53b7ead695b2ffbdea0ee82cdbb97c1277
Geovanni Oliveira <geovannirock@gmail.com>
2017-07-27 18:53:59 -0300

    Construction of the view structure

---
+++ Umbrella/Umbrella/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  Umbrella
+//
+//  Created by Geovanni Oliveira de Jesus on 27/07/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
---
+++ Umbrella/Umbrella/TabBarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarViewController.swift
+//  Umbrella
+//
+//  Created by Geovanni Oliveira de Jesus on 27/07/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+}
