fe265771ffaca5f5f164f8f61067983d46d3e2bf
Geovanni Oliveira <geovannirock@gmail.com>
2017-07-28 18:40:30 -0300

    registering a report

---
+++ Umbrella/Umbrella/FirebaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseManager.swift
+//  Umbrella
+//
+//  Created by Geovanni Oliveira de Jesus on 28/07/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+import Foundation
