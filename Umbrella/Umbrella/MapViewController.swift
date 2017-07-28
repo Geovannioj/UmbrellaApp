ee43d3adaec55ff8829b57c5b1f47c2a3bd35212
EduardoTorres <eduardo.torres@hotmail.com>
2017-07-28 18:05:30 -0300

    Location request, user tracking working.

--- Umbrella/Umbrella/MapViewController.swift
+++ Umbrella/Umbrella/MapViewController.swift
@@ -8,7 +8,56 @@
 
 import Foundation
 import UIKit
+import CoreLocation
+import Mapbox
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MGLMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authorizationStatusCheck()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+        
+    func configLocationManager(locationManager:CLLocationManager){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
+    }
+    func authorizationStatusCheck(){
+        if (CLLocationManager.authorizationStatus() != .authorizedWhenInUse){
+            locationManager.requestWhenInUseAuthorization()
+            
+        }else{
+            configLocationManager(locationManager: locationManager)
+        }
+        
+    }
+    
+    func centerOnUser(){
+       // let camera = MGLMapCamera(lookingAtCenter: (mapView.userLocation?.coordinate)!, fromDistance: CLLocationDistance.init(exactly: 500)!, pitch: 5, heading: CLLocationDirection(exactly: 9)!)
+       // mapView.setCamera(camera, animated: false)
+        mapView.setCenter((locationManager.location?.coordinate)!, zoomLevel: 13, animated: true)
+        mapView.showsUserLocation = true
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+           // configLocationManager(locationManager: locationManager)
+            locationManager.startUpdatingLocation()
+            centerOnUser()
+        }
+    }
     
 }
+
+    
+    
+
---
+++ Umbrella/Umbrella/MapViewDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewDelegate.swift
+//  Umbrella
+//
+//  Created by Eduardo Pereira on 28/07/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MapViewDelegate: NSObject,MGLMapViewDelegate {
+    
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        
+        return nil
+    }
+    
+    
+
+}
