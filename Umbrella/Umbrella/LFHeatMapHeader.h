0ba991274d282f947450a33122a5f2ad62cf1a93
EduardoTorres <eduardo.torres@hotmail.com>
2017-08-07 17:51:20 -0300

    removed things

--- Umbrella/Umbrella/LFHeatMapHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  LFHeatMapHeader.h
-//  Umbrella
-//
-//  Created by Eduardo Pereira on 01/08/17.
-//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
-//
-
-#import <LFHeatMap/LFHeatMap-umbrella.h>
