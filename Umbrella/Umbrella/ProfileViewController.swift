cd77f2b8e0e29f5c1bfc768d80422765808adc32
Geovanni Oliveira <geovannirock@gmail.com>
2017-08-13 18:32:46 -0300

    structure profile screen - view profile - View reports - settings screen

---
+++ Umbrella/Umbrella/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Umbrella
+//
+//  Created by Geovanni Oliveira de Jesus on 13/08/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
